7c87c39169c66f06c01a53219fd3be45fe402e45
psynt <psynt@nottingham.ac.uk>
2017-05-31 23:10:51 +0100

    bucketed

--- android/src/me/monokuma/game/AndroidLauncher.java
+++ android/src/me/monokuma/game/AndroidLauncher.java
@@ -1,10 +1,8 @@
 package me.monokuma.game;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import me.monokuma.game.Zergo;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override

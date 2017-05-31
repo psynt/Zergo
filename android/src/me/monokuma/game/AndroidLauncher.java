b4b35050d8679d3a33c0127d559bbd97df6c8ec8
psynt <psynt@nottingham.ac.uk>
2017-05-31 19:04:25 +0100

    init commit

---
+++ android/src/me/monokuma/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package me.monokuma.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import me.monokuma.game.Zergo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Zergo(), config);
+	}
+}
---
+++ desktop/src/me/monokuma/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.monokuma.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.monokuma.game.Zergo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Zergo(), config);
+	}
+}
